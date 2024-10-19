40077148872dfa61ab0b94b9727b28546910ecba
bohdandemchukk bc2024-3 <bogdanchuk2006@gmail.com>
2024-10-19 20:06:39 +0300

    виконав першу частину

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const { program } = require('commander');
+
 program
     .requiredOption('-i, --input <path>', 'Path to input file')
     .option('-o, --output <path>', 'Path to output file')
@@ -13,6 +14,7 @@ if (!options.input) {
     console.error('Please, specify input file');
     process.exit(1);
 }
+
 if (!fs.existsSync(options.input)) {
     console.error('Cannot find input file');
     process.exit(1);
@@ -20,35 +22,23 @@ if (!fs.existsSync(options.input)) {
 
 let inputData;
 try {
-    const data = fs.readFileSync(options.input, 'utf8');
+    const data = fs.readFileSync(options.input);
     inputData = JSON.parse(data);
-} catch (error) {
+} 
+catch (error) {
     console.error('Error reading file:', error);
     process.exit(1);
 }
-function findMinReserve(data) {
-    let minReserve = data[0];
-
-    for (const item of data) {
-        if (item.value < minReserve.value) {
-            minReserve = item;
-        }
-    }
-
-    return minReserve;
-}
-
-const minReserve = findMinReserve(inputData);
-const result = `${minReserve.txt}:${minReserve.value}`;
 
 if (options.output) {
     try {
-        fs.writeFileSync(options.output, result, 'utf8');
+        fs.writeFileSync(options.output, JSON.stringify(inputData, null, 1));
         console.log(`Result written to ${options.output}`);
     } catch (error) {
         console.error('Error writing to file:', error);
     }
 }
+
 if (options.display) {
-    console.log('Result:', result);
+    console.log('Result:', inputData);
 }
